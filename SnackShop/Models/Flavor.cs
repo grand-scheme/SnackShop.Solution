1457fb251e67287ccf40958894a6a882a443ca5c
Shannon Grantski <grantski@pm.me>
2021-01-15 13:18:24 -0800

    Add DisplayNames and ICollections to models

--- SnackShop/Models/Flavor.cs
+++ SnackShop/Models/Flavor.cs
@@ -1,11 +1,20 @@
-using System.Collections.Generic; // if lists
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace SnackShop.Models
 {
 	public class Flavor
 	{
+		public Flavor()
+		{}
 		public int FlavorId {get;set;}
+		[DisplayName("Flavor:")]
 		public string FlavorName {get;set;}
+		[DisplayName("Available from")]
 		public string FlavorTimeOfDay {get;set;}
+
+		public virtual ICollection<TreatFlavor> Treats {get;set;}
 	}
 }
--- SnackShop/Models/Treat.cs
+++ SnackShop/Models/Treat.cs
@@ -1,13 +1,24 @@
-using System.Collections.Generic; // if lists
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace SnackShop.Models
 {
 	public class Treat
 	{
+		public Treat()
+		{}
 		public int TreatId {get; set;}
+		[DisplayName("Item:")]
 		public string TreatName  {get; set;}
+		[DisplayName("Calories:")]
 		public int TreatCalories {get; set;}
+		[DisplayName("In Stock:")]
 		public bool TreatInStock {get; set;}
+		[DisplayName("Price:")]
 		public double TreatPrice  {get; set;}
+
+		public virtual ICollection<TreatFlavor> Flavors {get;set;}
 	}
 }
