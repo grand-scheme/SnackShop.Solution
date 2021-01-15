2816ffc80e5c4731bedcacd121ce4f3b69747049
Shannon Grantski <grantski@pm.me>
2021-01-15 13:58:26 -0800

    add using statements

--- SnackShop/Controllers/TreatsController.cs
+++ SnackShop/Controllers/TreatsController.cs
@@ -1,1 +1,37 @@
-using Microsoft.AspNetCore.Mvc;using SnackShop.Models;using System.Collections.Generic;namespace SnackShop.Controllers{	public class TreatsController : Controller	{		[HttpGet("/Treats")]		public ActionResult Index()		{			return View();		}	}}
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using SnackShop.Models;
+using System.Collections.Generic;
+
+namespace SnackShop.Controllers
+{
+	public class TreatsController : Controller
+	{
+		private readonly SnackShopContext _db;
+		public TreatsController(SnackShopContext db)
+		{
+			_db = db;
+		}
+
+		public ActionResult Index()
+		{
+			return View();
+		}
+	// create
+	// create2
+	// read
+	// 
+	// read all
+	// edit
+	// edit 2
+	// delete
+	// delete2
+	// delete all
+	// add flavor
+	// add flavor 2
+	// delete flavor
+	// delete all flavors
+	}
+}
