db6ca8b91b85fd6c9cf44764810a58abc1901a0b
Shannon Grantski <grantski@pm.me>
2021-01-18 22:15:02 -0800

    update database

---
+++ SnackShop/Migrations/20210119060252_addAccount.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SnackShop.Migrations
+{
+    public partial class addAccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
