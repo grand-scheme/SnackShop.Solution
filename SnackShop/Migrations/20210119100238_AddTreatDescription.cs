bfdc5316c58c3ec692f21f6596ad20a6cef26f3b
Shannon Grantski <grantski@pm.me>
2021-01-19 02:32:55 -0800

    update database

---
+++ SnackShop/Migrations/20210119100238_AddTreatDescription.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SnackShop.Migrations
+{
+    public partial class AddTreatDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TreatDescription",
+                table: "Treats",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TreatDescription",
+                table: "Treats");
+        }
+    }
+}
