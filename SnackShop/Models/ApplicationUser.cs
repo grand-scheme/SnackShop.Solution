490ea945caa85165c58a7d64e4972c295c6f482d
Shannon Grantski <grantski@pm.me>
2021-01-18 21:35:52 -0800

    begin adding Identity support to project

---
+++ SnackShop/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace SnackShop.Models
+{
+  public class ApplicationUser : IdentityUser
+  {
+    
+  }
+}
--- SnackShop/Models/SnackShopContext.cs
+++ SnackShop/Models/SnackShopContext.cs
@@ -1,5 +1,11 @@
-using Microsoft.EntityFrameworkCore;namespace SnackShop.Models{	public class SnackShopContext : DbContext{
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SnackShop.Models{
+	public class SnackShopContext : IdentityDbContext<ApplicationUser>{
 		public DbSet<Treat> Treats {get; set;}
 		public DbSet<Flavor> Flavors {get; set;}
 		public DbSet<TreatFlavor> TreatFlavor {get;set;}
-		public SnackShopContext(DbContextOptions options) : base(options) { }	}}
+		public SnackShopContext(DbContextOptions options) : base(options) { }
+	}
+}
